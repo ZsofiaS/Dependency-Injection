a70247faad9db5f8e7187e5a05ae18faa5a5d45e
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-22 11:23:14 +0000

    First commit: Greeter (introduction)

---
+++ Greeter/lib/greeter.rb
@@ -0,0 +1,15 @@
+class Greeter
+  def initialize(smiley = Smiley.new)
+    @smiley = smiley
+  end
+  
+  def greet
+    "Hello #{@smiley.get}"
+  end
+end
+
+class Smiley
+  def get
+    ":)"
+  end
+end
